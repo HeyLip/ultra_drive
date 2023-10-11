9a98321a9ececfcd3115411eaa40b3f829dce195
HeyLip <dlgkfla007@gmail.com>
2023-10-11 15:38:25 +0900

    only_gostop

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/harim/xycar_ws/src/ultra_drive
+# Build directory: /home/harim/xycar_ws/src/ultra_drive/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
